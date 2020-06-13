43cc679732b9336e79a8e5793c48a30f6ddadeff
sun198986 <334828328@qq.com>
2020-06-14 06:43:12 +0800

    修改类

--- Station.Entities/DB2Admin/Regist.cs
+++ Station.Entities/DB2Admin/Regist.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Station.Common;
 
@@ -8,166 +9,167 @@ namespace Station.Entities.DB2Admin
     [Table("DB2Admin.Regist")]
     public class Regist
     {
+        [Key, Column(Order = 0)]
         public string RegistId { set; get; }
         
         public DateTime RegistDate { set; get; }
         
-        public string MaintainNumber { set; get; }
-        public string OM1 { set; get; }
+       // public string MaintainNumber { set; get; }
+       // public string OM1 { set; get; }
         
-        public string CustomerName { set; get; }
+       // public string CustomerName { set; get; }
         
-        public string CustomerAddress { set; get; }
+       // public string CustomerAddress { set; get; }
         
-        public string CustomerLinkman { set; get; }
+       // public string CustomerLinkman { set; get; }
         
-        public string CustomerPhone { set; get; }
+       // public string CustomerPhone { set; get; }
         
-        public string CustomerFax { set; get; }
+       // public string CustomerFax { set; get; }
         
-        public int ProductTypeID { set; get; }
+       // public int ProductTypeID { set; get; }
         
-        public string ModelCode { set; get; }
+       // public string ModelCode { set; get; }
         
-        public string ModelName { set; get; }
+       // public string ModelName { set; get; }
         
-        public string SerialNumber { set; get; }
+       // public string SerialNumber { set; get; }
         
-        public string WarrantyNo { set; get; }
+       // public string WarrantyNo { set; get; }
         
-        public string GlobalEncoding { set; get; }
+       // public string GlobalEncoding { set; get; }
         
-        public int GroupID { set; get; }
+       // public int GroupID { set; get; }
         
-        public string NcModelCode { set; get; }
+       // public string NcModelCode { set; get; }
         
-        public string NcSerialNumber { set; get; }
+       // public string NcSerialNumber { set; get; }
         
-        public string EquipmentSeries { set; get; }
+       // public string EquipmentSeries { set; get; }
         
-        public string ManufacturerName { set; get; }
+       // public string ManufacturerName { set; get; }
         
-        public string EquipmentModel { set; get; }
+       // public string EquipmentModel { set; get; }
         
-        public string EquipmentSerial { set; get; }
+       // public string EquipmentSerial { set; get; }
         
-        public DateTime InstallationDate { set; get; }
+       // public DateTime InstallationDate { set; get; }
         
-        public int ReturnInstructID { set; get; }
-        public DateTime OM2 { set; get; }
+       // public int ReturnInstructID { set; get; }
+       // public DateTime OM2 { set; get; }
         
-        //public EWarrantyState WarrantyState { set; get; }
+       // //public EWarrantyState WarrantyState { set; get; }
         
-        public DateTime FaultDateTime { set; get; }
+       // public DateTime FaultDateTime { set; get; }
         
-        public string Engineer { set; get; }
+       // public string Engineer { set; get; }
         
-        public string PartExchange { set; get; }
+       // public string PartExchange { set; get; }
         
-        public string FaultDescription { set; get; }
+       // public string FaultDescription { set; get; }
         
-        public string CustomerRequest { set; get; }
+       // public string CustomerRequest { set; get; }
         
-        public int FrequencyID { set; get; }
+       // public int FrequencyID { set; get; }
         
-        public ETemperature TemperatureID { set; get; }
+       // public ETemperature TemperatureID { set; get; }
         
-        public bool IsCharge { set; get; }
+       // public bool IsCharge { set; get; }
         
-        public bool IsPayed { set; get; }
+       // public bool IsPayed { set; get; }
         
-        public string CreatedUser { set; get; }
+       // public string CreatedUser { set; get; }
         
-        public string CreatedUserName { set; get; }
+       // public string CreatedUserName { set; get; }
         
-        public DateTime CreatedDateTime { set; get; }
+       //// public DateTime CreatedDateTime { set; get; }
         
-        public string UpdatedUser { set; get; }
+       // public string UpdatedUser { set; get; }
         
-        public DateTime UpdatedDateTime { set; get; }
+       // public DateTime UpdatedDateTime { set; get; }
         
-        public int ItemsSourceID { set; get; }
-        //
-        //public bool IsPrimary { set; get; }
+       // public int ItemsSourceID { set; get; }
+       // //
+       // //public bool IsPrimary { set; get; }
         
-        public string PAppAdvice { set; get; }
+       // public string PAppAdvice { set; get; }
         
-        public string PAppUser { set; get; }
+       // public string PAppUser { set; get; }
         
-        public DateTime? PAppDateTime { set; get; }
+       // public DateTime? PAppDateTime { set; get; }
         
-        public string PUpdatedUser { set; get; }
+       // public string PUpdatedUser { set; get; }
         
-        public DateTime? PUpdatedDateTime { set; get; }
+       // public DateTime? PUpdatedDateTime { set; get; }
         
-        public bool IsRepaired { set; get; }
+       // public bool IsRepaired { set; get; }
         
-        public DateTime? RepairedDate { set; get; }
+       // public DateTime? RepairedDate { set; get; }
         
-        public bool IsMaintain { set; get; }
+       // public bool IsMaintain { set; get; }
         
-        public DateTime? MaintainDate { set; get; }
+       // public DateTime? MaintainDate { set; get; }
         
-        public bool IsQuotation { set; get; }
+       // public bool IsQuotation { set; get; }
         
-        public DateTime? QuotationDate { set; get; }
+       // public DateTime? QuotationDate { set; get; }
         
-        public bool IsShipping { set; get; }
+       // public bool IsShipping { set; get; }
         
-        public DateTime? ShippingDate { set; get; }
+       // public DateTime? ShippingDate { set; get; }
         
-        public ESubmitState SubmitState { set; get; }
+       // public ESubmitState SubmitState { set; get; }
         
-        public DateTime? WarrantyStartDate { set; get; }
+       // public DateTime? WarrantyStartDate { set; get; }
         
-        public DateTime? WarrantyEndDate { set; get; }
+       // public DateTime? WarrantyEndDate { set; get; }
         
-        public EStationAppState PAppState { set; get; }
-        //
-        //public bool IsFinally { set; get; }
+       // public EStationAppState PAppState { set; get; }
+       // //
+       // //public bool IsFinally { set; get; }
         
-        public EStationAppState FAppState { set; get; }
+       // public EStationAppState FAppState { set; get; }
         
-        public string FAppAdvice { set; get; }
+       // public string FAppAdvice { set; get; }
         
-        public string FAppUser { set; get; }
+       // public string FAppUser { set; get; }
         
-        public DateTime? FAppDateTime { set; get; }
+       // public DateTime? FAppDateTime { set; get; }
         
-        public string FUpdatedUser { set; get; }
+       // public string FUpdatedUser { set; get; }
         
-        public DateTime? FUpdatedDateTime { set; get; }
+       // public DateTime? FUpdatedDateTime { set; get; }
         
-        public string ProductOrigin { set; get; }
+       // public string ProductOrigin { set; get; }
         
-        public string Other { set; get; }
+       // public string Other { set; get; }
         
-        public DateTime? SendDate { set; get; }
+       // public DateTime? SendDate { set; get; }
         
-        public bool IsReturn { set; get; }
+       // public bool IsReturn { set; get; }
         
-        public bool IsClosed { set; get; }
+       // public bool IsClosed { set; get; }
         
-        public int StationID { set; get; }
+       // public int StationID { set; get; }
         
-        public string StationNo { set; get; }
+       // public string StationNo { set; get; }
         
-       // public List<StationRegistAccessoryInfo> Accessories { set; get; }
+       //// public List<StationRegistAccessoryInfo> Accessories { set; get; }
 
-        public string Group { set; get; }
+       // public string Group { set; get; }
         
-        public string ProductType { set; get; }
+       // public string ProductType { set; get; }
         
-        public string StationName { set; get; }
+       // public string StationName { set; get; }
         
-        public string SaleOrgan { set; get; }
+       // public string SaleOrgan { set; get; }
         
-        public decimal TotalAmount { set; get; }
+       // public decimal TotalAmount { set; get; }
         
-        public string ReturnState { set; get; }
+       // public string ReturnState { set; get; }
 
-        public bool AccessoryEdit { set; get; }
-        public bool QuotationEdit { set; get; }
-        public bool MaintainEdit { set; get; }
+       // public bool AccessoryEdit { set; get; }
+       // public bool QuotationEdit { set; get; }
+       // public bool MaintainEdit { set; get; }
     }
 }
