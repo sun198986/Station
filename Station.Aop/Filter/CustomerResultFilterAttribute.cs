f982ea2c3499662fbbdba72f15e6a0c0d6d7481d
sun198986 <334828328@qq.com>
2020-06-15 13:43:23 +0800

    添加筛选器

---
+++ Station.Aop/Filter/CustomerResultFilterAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Station.Aop.Filter
+{
+    public class CustomerResultFilterAttribute:ResultFilterAttribute
+    {
+        //验证客户端是否已经获取了数据
+        public override void OnResultExecuted(ResultExecutedContext context)
+        {
+            base.OnResultExecuted(context);
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            base.OnResultExecuting(context);
+        }
+    }
+}
